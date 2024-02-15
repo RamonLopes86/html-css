937af5ecb5bff478c7504f4ed7a55545f9be786e
Ramon Lopes <dev.ramondasilvalopes@gmail.com>
2024-02-15 13:22:43 -0300

    atualização do exercicio 26

---
+++ ex026/mq005/projeto.js
@@ -0,0 +1,20 @@
+
+let menu = document.getElementById('menu-opcoes');
+
+function clicar(){
+
+
+if(menu.style.display == "block"){
+
+    menu.style.display=" none"
+    
+    
+}else{
+
+    menu.style.display="block"
+   
+   
+}
+    
+}
+
